99e533116dcee3bcefdd90c52f25d9e081624485
themylogin <themylogin@gmail.com>
2014-06-09 01:05:26 +0700

    Fix 404 errors from restful API getting into sentry

--- last_fm/app.py
+++ last_fm/app.py
@@ -3,10 +3,12 @@ from __future__ import absolute_import, unicode_literals
 
 from flask import Flask
 from raven.contrib.flask import Sentry
+from werkzeug.exceptions import HTTPException
 
 import last_fm.config
 
 app = Flask("last_fm")
 app.config.from_object(last_fm.config)
+app.config["RAVEN_IGNORE_EXCEPTIONS"] = [HTTPException]
 
 sentry = Sentry(app)
