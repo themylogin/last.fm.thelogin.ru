e22d7115a722b5fd9a740dc496736730f0c219e3
themylogin <themylogin@gmail.com>
2015-03-10 19:20:28 +0600

    Fix bug which made running with reloader impossible

--- last_fm/__main__.py
+++ last_fm/__main__.py
@@ -4,8 +4,16 @@ from __future__ import absolute_import, division, unicode_literals
 from datetime import datetime
 import os
 import subprocess
+import sys
 from texttable import Texttable
 from urlparse import urlparse
+
+# When we run application with werkzeug reloader, it transfers `python -m appname` into `python .../appname/__main__.py`
+# which adds app directory into sys.path which makes it impossible to create modules with common names like "celery"
+# because then other packages using the same module name (e.g. themyutils.celery.beat) will fail to import because
+# they will try to import from appname.celery first. This is why app directory should not be in sys.path
+if os.path.dirname(__file__) in sys.path:
+    sys.path.remove(os.path.dirname(__file__))
 
 from themyutils.sqlalchemy.sql import literal_query
 
